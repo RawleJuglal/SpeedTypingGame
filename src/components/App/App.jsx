596098c6e3dbcf447308c35c39bf83e84a70e29c
Rawle Juglal <rawledjuglal@gmail.com>
2022-12-30 19:06:01 -0600

    moved game logic
    created hooks useWordGame for logic and moved out of component.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,74 +1,9 @@
 import React, {useState, useRef} from 'react'
 import './App.css'
+import useWordGame from '../../hooks/useWordGame'
 
 function App() {
-  const [text, setText] = useState({textContent:''})
-  const [timeRemaining, setTimeRemaining] = useState(5)
-  const [start, setStart] = useState(false)
-  const [words, setWords] = useState(0)
-  const textBoxRef = useRef(null)
-    
-  React.useEffect(()=>{
-      if(start && timeRemaining > 0){
-          setTimeout(() => {
-          removeOneSecond(timeRemaining)
-          }, "1000") 
-      } else if (timeRemaining === 0){
-        endGame()
-      }
-      
-      
-  },[timeRemaining, start])
-
-  
-
-  function handleChange(e){
-    const {value} = e.target
-    setText(()=>{
-      return {textContent:value}
-    })
-  }
-
-  function calcNumberOfWords(text){
-      const arr = text.trim().split(' ')
-      console.log(arr.filter(word => word !== '').length )
-      return arr.filter(word => word !== '').length  
-  }
-
-  function updateWords(num){
-    setWords(num)
-    return true
-  }
-
-
-  function removeOneSecond(time){
-      const newTime = time -1;
-      setTimeRemaining(newTime)
-  }
-
-  function resetRemainingTime(){
-    setTimeRemaining(5)
-  }
-
-  function resetTextBox(){
-    setText(()=>{
-      return {textContent:''}
-    })
-  }
-
-  function startGame(e){
-    e.preventDefault()
-    setStart(true)
-    textBoxRef.current.disabled = false
-    textBoxRef.current.focus()
-  }
-
-  function endGame(){
-    setStart(false)
-    updateWords(calcNumberOfWords(text.textContent))
-    resetRemainingTime(5)
-    resetTextBox()
-  }
+  const {textBoxRef, start, text, handleChange, timeRemaining, startGame, words} = useWordGame()
 
   return (
     <div className="--app-app-container">
